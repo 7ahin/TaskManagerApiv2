93766f6530baf0931362afff084437c860a7c3cd
7ahin <muhdzahin29@Gmail.com>
2026-02-20 06:51:15 +0800

    Edit Program.cs

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoL
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
