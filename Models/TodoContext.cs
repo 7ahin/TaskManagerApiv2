ddd4bb50fa7b659f62aecd401e4fe9a0207cbcbe
7ahin <muhdzahin29@Gmail.com>
2026-02-20 06:45:36 +0800

    Initial commit

---
+++ Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagerApi.Models
+{
+    // Inheriting from DbContext allows us to interact with the database
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        // This property represents our table of items
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskManagerApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
