bc4e485ec34e01a0005357f982008d2ef14caa2e
7ahin <muhdzahin29@Gmail.com>
2026-02-22 10:23:06 +0800

    Update TodoItem.cs

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -5,5 +5,8 @@
         public long Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+
+        public string? Priority { get; set; }
+        public DateTime? DueDate { get; set; }
     }
 }
