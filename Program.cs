79d34c23b8566e6f3553863ae52cd56271d8d311
7ahin <muhdzahin29@Gmail.com>
2026-03-18 14:26:22 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System;
 using TaskManagerApi.Models;
 
 const string AllowFrontendOrigin = "AllowFrontendOrigin";
@@ -10,9 +11,16 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: AllowFrontendOrigin,
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
+            var origins = builder.Configuration["AllowedOrigins"]?
+                .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries) ?? Array.Empty<string>();
+            if (origins.Length > 0)
+            {
+                policy.WithOrigins(origins).AllowAnyHeader().AllowAnyMethod();
+            }
+            else
+            {
+                policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+            }
         });
 });
 // Add services to the container.
